0d37977cba41af2274f725ea3292802b34176a40
Igor_Aukhadeev <rumbks@gmail.com>
2023-03-13 18:39:43 +0500

    Fix linter

--- page_analyzer/db/connection.py
+++ page_analyzer/db/connection.py
@@ -3,20 +3,22 @@ from flask import Flask, g
 
 from page_analyzer.environ import env
 
+
 def teardown_db(exception):
     db_connection = g.pop('db', None)
 
     if db_connection is not None:
         db_connection.close()
 
+
 def setup(app: Flask) -> None:
     app.teardown_appcontext(teardown_db)
+
 
 def _get_connection():
     if 'db_connection' not in g:
         g.db_connection = psycopg2.connect(env("DATABASE_URL"))
     return g.db_connection
-
 
 
 def get_cursor():
