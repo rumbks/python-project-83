6736a03c170ae7c9d29f1541d83a449ee2a5dda6
Igor_Aukhadeev <rumbks@gmail.com>
2023-03-13 14:26:15 +0500

    Close db connection

--- page_analyzer/db/__init__.py
+++ page_analyzer/db/__init__.py
@@ -1,1 +1,2 @@
 from page_analyzer.db.entities import url  # noqa: F401
+from page_analyzer.db import connection  # noqa: F401
--- page_analyzer/db/connection.py
+++ page_analyzer/db/connection.py
@@ -1,17 +1,29 @@
 import psycopg2
-from flask import Flask, current_app
+from flask import Flask, g
 
 from page_analyzer.environ import env
 
+def teardown_db(exception):
+    db_connection = g.pop('db', None)
+
+    if db_connection is not None:
+        db_connection.close()
 
 def setup(app: Flask) -> None:
-    connection = psycopg2.connect(env("DATABASE_URL"))
-    app.db_connection = connection
+    app.teardown_appcontext(teardown_db)
+
+def _get_connection():
+    if 'db_connection' not in g:
+        g.db_connection = psycopg2.connect(env("DATABASE_URL"))
+    return g.db_connection
+
 
 
 def get_cursor():
-    return current_app.db_connection.cursor()
+    connection = _get_connection()
+    return connection.cursor()
 
 
 def commit() -> None:
-    current_app.db_connection.commit()
+    connection = _get_connection()
+    connection.commit()
